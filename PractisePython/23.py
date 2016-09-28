7799e63ecc015fda860c4514b3bae4f089bd3578
FabianFalck <fabian.falck@web.de>
2016-09-29 00:15:29 +0800

    I/O 2

--- PractisePython/23.py
+++ PractisePython/23.py
@@ -1,14 +1,35 @@
+#strip() doesn't actually change here anything, since the data is cleaned.
+#The error didn't lie in the strip, it was a download problem of the txt file.
+
 prime_numbers = []
 happy_numbers = []
 
 with open('PythonDrills/PractisePython/primenumbers.txt','r') as prime_file:
-    line = prime_file.readline()
+    line = prime_file.readline().strip()
     while line:
-        prime_numbers.append(int(line))
-        line = prime_file.readline()
+        if line != '':
+            prime_numbers.append(int(line))
+        line = prime_file.readline().strip()
 
 with open('PythonDrills/PractisePython/happynumbers.txt','r') as happy_file:
-    line = happy_file.readline()
+    print line
+    line = happy_file.readline().strip()
     while line:
-        happy_numbers.append(int(line))
-        line = happy_file.readline()
+        if line != '':
+            happy_numbers.append(int(line))
+        line = happy_file.readline().strip()
+
+print prime_numbers
+print happy_numbers
+
+overlap = []
+
+for p in prime_numbers:
+    if p in happy_numbers:
+        overlap.append(p)
+        continue
+
+#using smooth and nice list comprehensions --> try to use them as often as possible
+#overlaplist = [elem for elem in primeslist if elem in happieslist]
+
+print overlap
