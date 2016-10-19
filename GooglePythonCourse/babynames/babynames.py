aebddf8a536ca98c459529bee0d734dc1cd5200d
FabianFalck <fabian.falck@web.de>
2016-10-19 21:40:41 +0800

    GoogleTutorial Day 2 Part 1

--- GooglePythonCourse/babynames/babynames.py
+++ GooglePythonCourse/babynames/babynames.py
@@ -27,12 +27,68 @@ Here's what the html looks like in the baby.html files:
 ...
 
 Suggested milestones for incremental development:
- -Extract the year and print it
- -Extract the names and rank numbers and just print them
- -Get the names data into a dict and print it
- -Build the [year, 'name rank', ... ] list and print it
- -Fix main() to use the extract_names list
+ -1 Extract the year and print it
+ -2 Extract the names and rank numbers and just print them
+ -3 Get the names data into a dict and print it
+ -4 Build the [year, 'name rank', ... ] list and print it
+ -5 Fix main() to use the extract_names list
 """
+
+#Step 1
+
+f = open(name = 'baby1990.html', mode = 'rU') #use 'rU' to read it as the actual string from an html, not some file reference
+f_string = f.read()
+
+match_year = re.search('Popularity\sin\s(\d\d\d\d)', f_string)
+if match_year:
+  print match_year.group(1)
+else:
+  print 'not found'
+
+#Step 2
+
+match_name = re.findall('<td>(\d+)</td><td>(\w+)</td><td>(\w+)', f_string)
+if match_name:
+  for triple in match_name:
+    print triple[0] + triple[1] + triple[2]
+
+# if match_name:
+#   print match_name.group(1)
+# else:
+#   print 'not found'
+
+#Step 3
+
+dict_1990 = {}
+if match_name:
+  for triple in match_name:
+    dict_1990[triple[1]] = triple[0]
+    dict_1990[triple[2]] = triple[0]
+
+dict_1990['Cierra']
+
+#Step 4
+
+list_1990 = []
+list_1990.append(match_year.group(1))
+for item in dict_1990.items():
+  list_1990.append(item[0] + ' ' + item[1])
+
+print list_1990
+
+#ordering
+
+def only_the_name(word_plus_number):
+  match = re.search('\S', word_plus_number)
+  return match.group()
+
+list_1990_ordered_without_first_number = sorted(list_1990[1:], key = only_the_name)
+
+list_1990_ordered = []
+list_1990_ordered.append(list_1990[0])
+list_1990_ordered = list_1990_ordered + list_1990_ordered_without_first_number
+
+print list_1990_ordered
 
 def extract_names(filename):
   """
@@ -40,34 +96,63 @@ def extract_names(filename):
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
-  res = []
+  f = open(name=filename, mode='rU')  # use 'rU' to read it as the actual string from an html, not some file reference
+  f_string = f.read()
 
-  file = open(name=filename, mode='r')
-  file_string = file.read()
+  match_year = re.search('Popularity\sin\s(\d\d\d\d)', f_string)
+  if match_year:
+    print match_year.group(1)
+  else:
+    print 'not found'
 
-  match = re.search(r'Popularity\sin\s(\d\d\d\d)', file_string)  # be careful: do not include whitespaces if not needed!
-  year = match.group(1)
+  # Step 2
 
-  list_findall = re.findall(r'<td>(\d+)</td>.*<td>(\w+)</td><td>(\w+)</td>',file_string)
-  dict = {}
-  for tuple in list_findall:
-    dict[tuple[1]] = tuple[0]
-    dict[tuple[2]] = tuple[0]
+  match_name = re.findall('<td>(\d+)</td><td>(\w+)</td><td>(\w+)', f_string)
+  if match_name:
+    for triple in match_name:
+      print triple[0] + triple[1] + triple[2]
 
+  # if match_name:
+  #   print match_name.group(1)
+  # else:
+  #   print 'not found'
 
+  # Step 3
 
-  return res
+  dict_1990 = {} #naming was not changed, should just be without the year number
+  if match_name:
+    for triple in match_name:
+      dict_1990[triple[1]] = triple[0]
+      dict_1990[triple[2]] = triple[0]
 
-#step 1
-match = re.search(r'Popularity\sin\s(\d\d\d\d)','<h3 align="center">Popularity in 1990</h3>') #be careful: do not include whitespaces if not needed!
-match.group(1)
+  dict_1990['Cierra']
 
-#step 2
-match_2 = re.search(r'<td>(\d+)</td>.*<td>(\w+)</td><td>(\w+)</td>','<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>')
-match_2.group(1,2,3)
+  # Step 4
 
-#step3
-file = open(name = filename, mode = 'r')
+  list_1990 = []
+  list_1990.append(match_year.group(1))
+  for item in dict_1990.items():
+    list_1990.append(item[0] + ' ' + item[1])
+
+  print list_1990
+
+  # ordering
+
+  def only_the_name(word_plus_number):
+    match = re.search('\S', word_plus_number)
+    return match.group()
+
+  list_1990_ordered_without_first_number = sorted(list_1990[1:], key=only_the_name)
+
+  list_1990_ordered = []
+  list_1990_ordered.append(list_1990[0])
+  list_1990_ordered = list_1990_ordered + list_1990_ordered_without_first_number
+
+  print list_1990_ordered
+
+  return
+
+extract_names('baby1990.html')
 
 def main():
   # This command-line parsing code is provided.
@@ -88,6 +173,23 @@ def main():
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
+  for filename in args:
+    names = extract_names(filename)
+
+    # Make text out of the whole list
+    text = '\n'.join(names)
+
+    if summary:
+      outf = open(filename + '.summary', 'w')
+      outf.write(text + '\n')
+      outf.close()
+    else:
+      print text
   
 if __name__ == '__main__':
   main()
+
+
+
+
+
